93a68521ee8fcda74ec14447aac9b98a4faab819
DonCosme <109149406+franema@users.noreply.github.com>
2022-09-22 10:17:44 -0300

    Simplifies sticky nav functionality

--- main.js
+++ main.js
@@ -5,17 +5,18 @@ setTimeout(() => {
 }, 150)
 
 function recalculateOffsetTop() {
+    window.scrollTo(0, 0)
     topOfNav = $nav.offsetTop
     return topOfNav
 }
 
 function fixNav () {
     if(this.scrollY >= topOfNav) {
-        document.body.style.paddingTop = `${$nav.offsetHeight}px`
+        // document.body.style.paddingTop = `${$nav.offsetHeight}px`
         $nav.classList.add("fixed-nav")
         
     } else {
-        document.body.style.paddingTop = 0
+        // document.body.style.paddingTop = 0
         $nav.classList.remove("fixed-nav")
     }
 }   
