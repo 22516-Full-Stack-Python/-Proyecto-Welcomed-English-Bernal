3faee7bd5b0ddb9976367190c7dc775abf34b5f9
DonCosme <109149406+franema@users.noreply.github.com>
2022-09-28 14:46:08 -0300

    Adds dropdown menu functionality

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
 const $nav = document.querySelector(".main")
 let topOfNav
-setTimeout(() => {
-    topOfNav = $nav.offsetTop
-}, 150)
+const $dropdown = document.querySelector(".dropdown")
+const $courses = document.querySelector("#courses")
 
+
+topOfNav = $nav.offsetTop
 function recalculateOffsetTop() {
     window.scrollTo(0, 0)
     topOfNav = $nav.offsetTop
     return topOfNav
 }
 
-function fixNav () {
-    if(this.scrollY >= topOfNav) {
-        // document.body.style.paddingTop = `${$nav.offsetHeight}px`
+function fixNav() {
+    if (this.scrollY >= topOfNav) {
         $nav.classList.add("fixed-nav")
-        
+
     } else {
-        // document.body.style.paddingTop = 0
         $nav.classList.remove("fixed-nav")
     }
-}   
+}
 
+function showDropdownMenu(e) {
+        $dropdown.classList.add("hovered")
+    
+}
+
+function hideDropdownMenu() {
+    $dropdown.classList.remove("hovered")
+}
+
+$courses.addEventListener("mouseenter", showDropdownMenu)
+$courses.addEventListener("mouseleave", hideDropdownMenu)
 window.addEventListener("scroll", fixNav)
 window.addEventListener("resize", recalculateOffsetTop)
 
