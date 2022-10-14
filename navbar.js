b9a8e4d7ce8d197cc33c4fe2aab32eefc7e91164
DonCosme <109149406+franema@users.noreply.github.com>
2022-10-14 11:43:48 -0300

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,22 +1,24 @@
 const $navbar = document.querySelector("nav")
 const $footer = document.querySelector("footer")
 
-$navbar.innerHTML = `<div class="nav-logo"></div>
-<div class="bar-container">
-    <div class="bar-item"><a href="index.html">Inicio</a></div>
-    <div class="bar-item"><a href="#">Nosotros</a></div>
-    <div class="bar-item" id="courses">Cursos
-        <div class="dropdown">
-            <a href="cursos.html">Adultos</a>
-            <a href="cursos.html">Adolescentes</a>
-            <a href="cursos.html">Niños</a>
-            <a href="cursos.html">Examenes</a>
+$navbar.innerHTML = `
+<div class="nav-logo"></div>
+    <div class="bar-container">
+        <div class="bar-item"><a href="index.html">Inicio</a></div>
+        <div class="bar-item"><a href="#">Nosotros</a></div>
+        <div class="bar-item" id="courses">Cursos
+            <div class="dropdown">
+                <a href="cursos.html">Adultos</a>
+                <a href="cursos.html">Adolescentes</a>
+                <a href="cursos.html">Niños</a>
+                <a href="cursos.html">Examenes</a>
+            </div>
         </div>
-    </div>
     <div class="bar-item"><a href="form.html">Contacto</a></div>
 </div>`
 
-$footer.innerHTML = ` <p>Copyright 2022 by. All rights reserved</p>
+$footer.innerHTML = ` 
+<p>Copyright 2022 by. All rights reserved</p>
 <div class="site-map">
     <a href="index.html">Inicio</a>
     <a href="">Nosotros</a>
