49a7339d753836865c9297eed925ccb02a3b0915
alejandrardecajal <alejandrardecajal@gmail.com>
2022-10-18 10:23:44 -0300

    Actualización textos de cursos

--- navbar.js
+++ navbar.js
@@ -5,13 +5,13 @@ $navbar.innerHTML = `
 <div class="nav-logo"></div>
     <div class="bar-container">
         <div class="bar-item"><a href="index.html">Inicio</a></div>
-        <div class="bar-item"><a href="#">Nosotros</a></div>
+        <div class="bar-item"><a href="nosotros.html">Nosotros</a></div>
         <div class="bar-item" id="courses">Cursos
             <div class="dropdown">
                 <a href="cursos.html">Niños</a>
                 <a href="cursos_adol.html">Adolescentes</a>
                 <a href="cursos_adult.html">Adultos</a>
-                <a href="cursos_cam.html">Examenes</a>
+                <a href="exam.html">Examenes</a>
             </div>
         </div>
     <div class="bar-item"><a href="form.html">Contacto</a></div>
@@ -21,7 +21,7 @@ $footer.innerHTML = `
 <p>Copyright 2022 by. All rights reserved</p>
 <div class="site-map">
     <a href="index.html">Inicio</a>
-    <a href="">Nosotros</a>
+    <a href="nosotros.html">Nosotros</a>
     <a href="cursos.html">Cursos</a>
     <a href="form.html">Contacto</a>
 </div>`
