6b68f57a70b88532c7137f6b7cb804ef17165a20
franema <franemarin@hotmail.com>
2022-10-23 15:57:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 const $startNow_Button = document.querySelector(".start-now_button")
 
-
-
 $startNow_Button.onclick = function () {
     location.href = "form.html"
 }
 
 
+const api = "https://api.quotable.io/random";
 
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+
+function changeFrase() {
+    fetch(api)
+        .then((res) => res.json())
+        .then((data) => {
+            quote.innerHTML = `"${data.content}"`;
+            author.innerHTML = `- ${data.author}`;
+        });
+}
+
+changeFrase()
+
+setInterval (function () {
+    changeFrase()
+}, 5000)
 
 
 
